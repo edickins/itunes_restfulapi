fe330638c4bfdda83c74105cff570be1ec04c5c7
ecdickins <ed.dickins@bleepbloop.net>
2022-08-24 16:12:34 +0100

    started work on pagination

--- api/controllers/playlistTracks.js
+++ api/controllers/playlistTracks.js
@@ -5,26 +5,48 @@ const Track = require('../models/Track');
 // @route GET /api/v1/playlists/:id
 // @token public
 exports.getPlaylistTracks = async (req, res) => {
-	const dbPlaylist = await Playlist.find({
+	/* 	const dbPlaylist = await Playlist.find({
 		playlistId: req.params.id,
-	});
+	}); */
 
-	const tracks = dbPlaylist[0].tracks;
+	try {
+		let query = Playlist.find({ playlistId: req.params.id });
 
-	const trackDetailPromises = tracks.map(track => {
-		return findTrackDetails(track);
-	});
+		const page = parseInt(req.query.page) || 1;
+		const pageSize = parseInt(req.query.limit) || 20;
+		const skip = (page - 1) * pageSize;
 
-	Promise.all(trackDetailPromises).then(results => {
-		const tracks = results.map(trackArray => {
-			return trackArray[0];
+		const dbPlaylist = await query;
+
+		const total = dbPlaylist[0].tracks.length;
+		const pages = Math.ceil(total / pageSize);
+		const endPoint = Math.min(skip + pageSize, total);
+		const tracks = dbPlaylist[0].tracks.slice(skip, endPoint);
+
+		const trackDetailPromises = tracks.map(track => {
+			return findTrackDetails(track);
 		});
-		res.status(200).json({
-			success: true,
-			msg: `get playlist with id ${req.params.id}`,
-			data: tracks,
+
+		Promise.all(trackDetailPromises).then(results => {
+			const tracks = results.map(trackArray => {
+				return trackArray[0];
+			});
+			res.status(200).json({
+				success: true,
+				msg: `get playlist with id ${req.params.id}`,
+				data: tracks,
+				count: tracks.length,
+				page,
+				pages,
+				total,
+			});
 		});
-	});
+	} catch (error) {
+		res.status(500).json({
+			success: 'error',
+			message: 'Server Error.',
+		});
+	}
 };
 
 function findTrackDetails(track) {
